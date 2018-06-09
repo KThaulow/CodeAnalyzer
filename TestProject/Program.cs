68d23bfec4e7c2622ddfc09b0d5e389436e1c73f
Kristian Thaulow <kristian1990@gmail.com>
2018-06-09 21:50:20 +0200

    Updated test project

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -2,13 +2,24 @@
 
 namespace TestProject
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int i = 1;
-            int j = 2;
-            int k = i + j;
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i = 1;
+			int j = 2;
+			int k = i + j;
+
+			var dateTime = DateTime.Now;
+
+			var dateTime2 = new DateTime(0, 0, 0, 0, 0, 0, DateTimeKind.Local);
+
+			MyMethod(DateTime.Now);
+		}
+
+
+		private static void MyMethod(DateTime dateTime)
+		{
+		}
+	}
 }
